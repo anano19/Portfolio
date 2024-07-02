9195b8e1cabcd9de58647cb7ba411fc021b9e4b3
anano19 <150928766+anano19@users.noreply.github.com>
2024-07-02 11:40:19 +0400

    Add files via upload

--- bank.py
+++ bank.py
@@ -5,6 +5,6 @@ name = name.lower()
 if name.startswith("hello"):
     print("$0")
 elif name.startswith("h"):
-    print("$20")    
+    print("$20")
 else:
-    print("$100")    
+    print("$100")
---
+++ emojize.py
@@ -0,0 +1,3 @@
+import emoji
+name = input("Input: ")
+print(emoji.emojize(name, language="alias"))
